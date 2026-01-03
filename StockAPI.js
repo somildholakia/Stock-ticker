0f085ea2a5d539c0e96041b4eb6b54fc30bf746c
somildholakia <somildholakia0852@gmail.com>
2026-01-03 20:21:08 +0530

    update

--- StockAPI.js
+++ StockAPI.js
@@ -3,11 +3,15 @@ const hours = now.getHours();
 const minutes = now.getMinutes();       
 const seconds = now.getSeconds();  
 
+function updatePrice(){
+   setInterval(1500,(Math.random() * 3).toFixed(2)) 
+}
+
 function getStockData(){
     return {
         name: 'QtechAI',
         sym: 'QTA',
-        price: (Math.random() * 3).toFixed(2),
+        price: updatePrice(),
         time: Date(`${hours} / ${minutes} / ${seconds}`)
     }
 }
