af94ffda0c16381ac2f40912f4ef65ecb94820a5
somildholakia <somildholakia0852@gmail.com>
2026-01-04 17:10:53 +0530

    update

--- index.js
+++ index.js
@@ -1,29 +1,15 @@
-import getStockData from "./StockAPI.js"
+import getStockData from "./StockAPI"
 
-setInterval(function () {
-    const stockData = getStockData()
-    renderStrockTicker(stockData)
-}, 1500)
-
-
+setInterval(function(){
+    const stockdata = getStockData
+    StockTicker(stockdata)
+})
 
 
-function renderStrockTicker(stockData) {
-
-    const stockDisplayName = document.getElementById("name")
-    const stockDisplaySymbol = document.getElementById("symbol")
-    const stockDisplayPrice = document.getElementById("price")
-    // const stockDisplayPriceIcon = document.getAnimations("price-icon")
-    const stockDisplayTime = document.getElementById("price-icon")
-
-    const { name, sym, price, time } = stockData
-
-    stockDisplayName.textContent = name
-    stockDisplaySymbol.textContent = sym
-    stockDisplayPrice.textContent = price
-    // stockDisplayPriceIcon =
-    stockDisplayTime.textContent = time
-    
-
+function StockTicker(stockdata){
+    const name = document.getElementById("name")
+    const symbol = document.getElementById("sym")
+    const price = document.getElementById("price")
+    const price_icon = document.getElementById("price-icon")
+    const time = document.getElementById("time")
 }
-
