e8a938c1501b5737c385ec6f5f2e478b466043c9
somildholakia <somildholakia0852@gmail.com>
2026-01-04 18:09:08 +0530

    update

--- index.js
+++ index.js
@@ -6,24 +6,36 @@ setInterval(function () {
 }, 1500)
 
 
-
+let previousPrice = null
 
 function renderStrockTicker(stockData) {
 
     const stockDisplayName = document.getElementById("name")
     const stockDisplaySymbol = document.getElementById("symbol")
     const stockDisplayPrice = document.getElementById("price")
-    // const stockDisplayPriceIcon = document.getAnimations("price-icon")
-    const stockDisplayTime = document.getElementById("price-icon")
+    const stockDisplayPriceIcon = document.getElementById("price-icon")
+    const stockDisplayTime = document.getElementById("time")
 
-    const { name, sym, price, time } = stockData
-
-    stockDisplayName.textContent = name
-    stockDisplaySymbol.textContent = sym
-    stockDisplayPrice.textContent = price
-    // stockDisplayPriceIcon =
-    stockDisplayTime.textContent = time
     
 
+    const { name, sym, price, time } = stockData
+    const priceIcon = document.createElement("span")
+
+    stockDisplayPriceIcon.innerHTML =""
+
+    if(price > previousPrice){
+        priceIcon.textContent = "priceTrend:" + "⬆️"
+    } else if(previousPrice > price){
+        priceIcon.textContent = "priceTrend:" + "⬇️"
+    }else {
+        priceIcon.textContent = "priceTrend:" + "➖"
+    }
+    stockDisplayName.textContent = "Name: " + name
+    stockDisplaySymbol.textContent = "Symbol: " + sym
+    stockDisplayPrice.textContent = "Price: " + price
+    stockDisplayPriceIcon.appendChild(priceIcon)   
+    stockDisplayTime.textContent = "Time: " + time
+    
+    previousPrice = price
 }
 
